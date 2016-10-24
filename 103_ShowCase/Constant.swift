ec1ec1672bd8460a722daca283a5e8cc3acf5374
salimi0225 <salimi0225@gmail.com>
2016-10-24 22:03:18 +0330

    Add Shadow to my "MaterialView" Class

---
+++ 103_ShowCase/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  103_ShowCase
+//
+//  Created by 09107110225 on 8/3/1395 AP.
+//  Copyright © 1395 Mohammad_salimi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+let SHADOW_COLOR :CGFloat = 157/255
--- 103_ShowCase/MaterialView.swift
+++ 103_ShowCase/MaterialView.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class MaterialView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        layer.cornerRadius = 2
+        layer.shadowColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.5).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        
+        
+        
+        
+        
     }
-    */
-
 }
