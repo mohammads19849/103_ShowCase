6924fefa63a94dfc949eeb7d3459f23e112dfa5e
salimi0225 <salimi0225@gmail.com>
2016-10-24 21:45:07 +0330

    create Material View

---
+++ 103_ShowCase/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  103_ShowCase
+//
+//  Created by 09107110225 on 8/3/1395 AP.
+//  Copyright © 1395 Mohammad_salimi. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
