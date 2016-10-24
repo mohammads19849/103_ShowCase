4bb0076a56a5415978d3f559cb25a44c18df5e57
salimi0225 <salimi0225@gmail.com>
2016-10-24 21:17:16 +0330

    Initial Commit

---
+++ 103_ShowCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  103_ShowCase
+//
+//  Created by 09107110225 on 8/3/1395 AP.
+//  Copyright © 1395 Mohammad_salimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
